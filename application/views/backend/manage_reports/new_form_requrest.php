426badedf20547b7ea72144b37b03edae5a01c2d
Chantouch <cs.appsaradev@gmail.com>
2018-01-09 22:46:59 +0700

    Added new form

---
+++ application/views/backend/manage_reports/new_form_requrest.php
@@ -0,0 +1,151 @@
+<div class="row" id="members">
+    <div class="col-xs-12">
+        <div class="text-center">
+            <h3>ព្រះរាជាណាចក្រកម្ពុជា</h3>
+            <h3>ជាតិ សាសនា ព្រះមហាក្សត្រ</h3>
+            <p>*******************</p>
+            <h3>ប្រវត្តិរូបសង្ខេប</3>
+        </div>
+        <div class="row">
+            <div class="col-xs-2">
+                <p>គោត្តនាម និង នាម</p>
+                <p>ថ្ងៃ ខែ ឆ្នាំកំណើត</p>
+                <p>ទីកន្លែងកំណើត</p>
+                <p>ថ្ងៃខែឆ្នាំចូលស្នាក់នៅ</p>
+                <p>អាសយដ្ឋានបច្ចុប្បន្ន</p>
+                <p>កំរិតវប្បធម៌</p>
+                <p>មុខងារបច្ចុប្បន្ន</p>
+            </div>
+            <div class="col-xs-10">
+                <p>ៈ<span class="letter-spacing-2">........................</span>ភេទ<span class="letter-spacing-2">........................</span>ជនជាតិ<span
+                            class="letter-spacing-2">........................</span>សញ្ជាតិ<span
+                            class="letter-spacing-2">........................</span></p>
+                <p>ៈ<span class="letter-spacing-2">........................</span></p>
+                <p>ៈ<span class="letter-spacing-2">........................</span></p>
+                <p>ៈ<span class="letter-spacing-2">........................</span></p>
+                <p>ៈ<span class="letter-spacing-2">........................</span></p>
+                <p>ៈ<span class="letter-spacing-2">........................</span></p>
+                <p>ៈ<span class="letter-spacing-2">........................</span></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-11 col-xs-offset-1">
+                <p>ក-ជាសិស្ស ឬ និស្សិត<span class="letter-spacing-2">........................</span>រៀននៅ<span
+                            class="letter-spacing-2">........................</span>ថ្នាក់ទី ឬ ឆ្នាំទី<span
+                            class="letter-spacing-2">........................</span></p>
+                <p>ជំនាន់ទី<span class="letter-spacing-2">........................</span>ក្រុមទី<span
+                            class="letter-spacing-2">........................</span>អាសយដ្ឋានសាលារៀន<span
+                            class="letter-spacing-2">........................</span></p>
+                <p>ខ-អ្នកធ្វើការ(មន្រ្តីរាជការ ឬ បុគ្គលិក)<span class="letter-spacing-2">........................</span>ឈ្មោះស្ថាប័ន<span
+                            class="letter-spacing-2">........................</span></p>
+                <p>អាសយដ្ឋានស្ថានប័នធ្វើការ<span class="letter-spacing-2">........................</span></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <p>ស្ថានភាពគ្រួសារ នៅលីវ ឬ មានគ្រួសារៈ<span class="letter-spacing-2">........................</span></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-11 col-xs-offset-1">
+                <p>ក-ឪពុកឈ្មោះ<span class="letter-spacing-2">........................</span>អាយុ<span
+                            class="letter-spacing-2">........................</span>មុខរបរ<span
+                            class="letter-spacing-2">........................</span></p>
+                <p>អាសយដ្ឋានបច្ចុប្បន្ន<span class="letter-spacing-2">........................</span></p>
+                <p>ខ-ម្ដាយឈ្មោះ<span class="letter-spacing-2">........................</span>អាយុ<span
+                            class="letter-spacing-2">........................</span>មុខរបរ<span
+                            class="letter-spacing-2">........................</span></p>
+                <p>អាសយដ្ឋានបច្ចុប្បន្ន<span class="letter-spacing-2">........................</span></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <p>មធ្យោបាយធ្វើដំណើរ ( កម្មសិទ្ធិផ្ទាល់ខ្លួន )</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-11 col-xs-offset-1">
+                <p>ក- រថយន្ដធុន<span class="letter-spacing-2">........................</span>ពណ៌<span
+                            class="letter-spacing-2">........................</span>ផ្លាកលេខ<span
+                            class="letter-spacing-2">........................</span>លេខតូ<span class="letter-spacing-2">........................</span>លេខម៉ាស៊ីន<span
+                            class="letter-spacing-2">........................</span></p>
+                <p>ខ- ម៉ូតូធុន<span class="letter-spacing-2">........................</span>ពណ៌<span
+                            class="letter-spacing-2">........................</span>ផ្លាកលេខ<span
+                            class="letter-spacing-2">........................</span>លេខតូ<span class="letter-spacing-2">........................</span>លេខម៉ាស៊ីន<span
+                            class="letter-spacing-2">........................</span></p>
+                <p>គ- កង់ប្រភេទ<span class="letter-spacing-2">........................</span>ពណ៌<span
+                            class="letter-spacing-2">........................</span></p>
+                <p>ប្រវត្តិរូបនេះពិតជាត្រឹមត្រូវ បើប្រាកចាកពីការពិត ខ្ញុំព្រះករុណាសូមទទួលខុសត្រូវចំពោះមុខច្បាប់ជាធរមាន
+                    ។</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-5 col-xs-offset-7">
+                <p>ធ្វើនៅ<span class="letter-spacing-2">..................</span>ថ្ងៃទី<span class="letter-spacing-2">.........</span>ខែ<span
+                            class="letter-spacing-2">...........</span>ឆ្នាំ២០១៦</p>
+                <p>ហត្ថលេខា និង ឈ្មោះ</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-11 col-xs-offset-1">
+                <div class="m-l-3">
+                    <p>អ្នកទទួលខុសត្រូវ</p>
+                    <p>មេកុដិលេខ<span class="letter-spacing-2">........................</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<hr/>
+<div style="text-align: center;">
+
+    <button class="btn btn-warning" type="button" id="btn-print" onclick="printDiv('members')">
+        <i class="ace-icon fa fa-print bigger-110"></i>
+        <?php echo $this->lang->line('fm_btn_print'); ?>
+    </button>
+
+</div>
+
+<style>
+
+    h1, h2, h3, h4, h5, h6 {
+        font-family: "Khmer", sans-serif;
+    }
+
+    .m-l-3 {
+        margin-left: 3rem;
+    }
+
+    .letter-spacing-2 {
+        letter-spacing: 0.2em;
+    }
+
+    @media print {
+
+        h1, h2, h3, h4, h5, h6 {
+            font-family: "Khmer", sans-serif !important;
+        }
+
+        .m-l-3 {
+            margin-left: 3rem;
+        }
+
+        .letter-spacing-2 {
+            letter-spacing: 0.2em;
+        }
+    }
+
+</style>
+
+<script type="text/javascript">
+    function printDiv(divName) {
+        //var divobj=document.getElementById('before_print');
+        //divobj.setAttribute('class','print_size');
+        var printContents = document.getElementById(divName).innerHTML;
+        var originalContents = document.body.innerHTML;
+        document.body.innerHTML = printContents;
+        window.print();
+        document.body.innerHTML = originalContents;
+    }
+</script>
